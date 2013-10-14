f4d21d6308afe81a02c79fe315438dfa371372ed
Michael Mester <m.mester@fu-berlin.de>
2013-10-14 18:12:00 +0200

    bgpd: rpki: apply gnu coding style

--- bgpd/bgp_main.c
+++ bgpd/bgp_main.c
@@ -440,9 +440,9 @@ main (int argc, char **argv)
   /* BGP related initialization.  */
   bgp_init ();
 
-#ifdef HAVE_RPKI
+  #ifdef HAVE_RPKI
   rpki_init();
-#endif
+  #endif
 
   /* Sort CLI commands. */
   sort_node ();
