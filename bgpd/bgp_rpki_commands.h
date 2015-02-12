5ed2060d5f2fb2cb567ae226d4ecbecd5c91589e
Nils Bars <nils.bars@haw-hamburg.de>
2015-02-12 12:32:00 +0100

    [bgpd][rpki] Update Quagga rpki impl
    Update the Quagga rpki implementation to match the changed API of
    rtrlib version 0.3.0.

--- bgpd/bgp_rpki_commands.h
+++ bgpd/bgp_rpki_commands.h
@@ -25,9 +25,9 @@
 #include "rtrlib/rtrlib.h"
 
 void install_cli_commands();
-rtr_mgr_group* get_rtr_mgr_groups();
+struct rtr_mgr_group* get_rtr_mgr_groups();
 unsigned int get_number_of_cache_groups();
-void free_rtr_mgr_groups(rtr_mgr_group* group, int length);
+void free_rtr_mgr_groups(struct rtr_mgr_group* group, int length);
 void delete_cache_group_list();
 
 #endif /* RPKI_COMMANDS_H_ */
