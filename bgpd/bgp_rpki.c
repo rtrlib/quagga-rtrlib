734779ca1bd23e5930312a54abff74835e82495e
Andreas Reuter <andreas.router@gmail.com>
2015-08-19 14:35:14 +0100

    Flag rpki_sync_socket_rtr as non-blocking to prevent blocking behaviour on quagga shutdown

--- bgpd/bgp_rpki.c
+++ bgpd/bgp_rpki.c
@@ -121,6 +121,7 @@ rpki_init_sync_socket()
     }
   rpki_sync_socket_rtr = fds[0];
   rpki_sync_socket_bgpd = fds[1];
+  fcntl(rpki_sync_socket_rtr, F_SETFL, O_NONBLOCK);
   thread_add_read(master, rpki_update_cb_sync_bgpd, 0, rpki_sync_socket_bgpd);
 }
 
